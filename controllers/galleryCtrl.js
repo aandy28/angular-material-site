0a62127ca752afe432a3f06d587b103df191de74
aandy28 <aandy28@googlemail.com>
2016-08-01 08:15:01 +0100

    stuffy stuff

--- controllers/galleryCtrl.js
+++ controllers/galleryCtrl.js
@@ -1,12 +1,26 @@
 'use strict';
 app.controller('GalleryCtrl', function($scope, $http, $stateParams, FlickrService) {
 	$scope.results = [];
+    $scope.slides = [];
 
 	$scope.photosetid = $stateParams.photosetid;
 	console.log($scope.photosetid);
     
     FlickrService.getAlbumPhotos($scope.photosetid).then(function(data){
         $scope.results = data;
+
+        for (var i = $scope.results.length - 1; i >= 0; i--) {
+            $scope.results[i]
+
+            var slide = {
+                src: 'https://farm'+$scope.results[i].farm+'.static.flickr.com/'+$scope.results[i].server+'/'+$scope.results[i].primary+'_'+$scope.results[i].secret+'_m.jpg',
+                w:500,
+                h:400,
+                pid: $scope.results[i].title._content
+            };
+
+            $scope.slides.push(slide);
+        }
     });
 
     // for (var i = $scope.results.photosets.photo.length - 1; i >= 0; i--) {
