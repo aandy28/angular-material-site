aa6e73ad7b955a55476cd3ba1f01ca20dc211e98
aandy28 <aandy28@googlemail.com>
2016-08-02 09:30:00 +0100

    fixed layout

--- controllers/galleryCtrl.js
+++ controllers/galleryCtrl.js
@@ -2,18 +2,22 @@
 app.controller('GalleryCtrl', function($scope, $http, $stateParams, FlickrService) {
     $scope.results = [];
     $scope.allslides = [];
-
     $scope.photosetid = $stateParams.photosetid;
-    var vm = this;
-    vm.title = 'ngPhotoswipe';
-
-    vm.opts = {
-        index: 0,
-        history:false
-    };
 
     // FlickrService.getAlbumPhotos($scope.photosetid).then(function(data){
     //     $scope.slides = data;
+
+    //     for (var i = data.photoset.photo.length - 1; i >= 0; i--) {
+
+    //         var slide = {
+    //             src: 'https://farm'+data.photoset.photo[i].farm+'.static.flickr.com/'+data.photoset.photo[i].server+'/'+data.photoset.photo[i].id+'_'+data.photoset.photo[i].secret+'_b.jpg',
+    //             w:500,
+    //             h:400,
+    //             pid: data.photoset.photo[i].id
+    //         };
+
+    //         $scope.allslides.push(slide);
+    //     }
 
     // });
 
@@ -42,29 +46,11 @@ app.controller('GalleryCtrl', function($scope, $http, $stateParams, FlickrServic
             };
 
             $scope.allslides.push(slide);
-
-
-
-
         }
-        // vm.slides = $scope.slides;
-        console.log(vm);
 
     }).error(function(error) {
         console.error(error);
 
     });
-    vm.showGallery = function(i) {
-        console.log("open gallery");
-        if (angular.isDefined(i)) {
-            vm.opts.index = i;
-        }
-        vm.open = true;
-    };
-
-    vm.closeGallery = function() {
-        console.log("close gallery");
-        vm.open = false;
-    };
-    vm.slides = $scope.allslides;
+    
 });
--- services/flickrService.js
+++ services/flickrService.js
@@ -1,43 +1,3 @@
-//function FlickrService($http, $sce, config) {
-
-// function albums() {
-//     return getData('posts?filter[category_name]=post');
-// }
-
-// function albumImages() {
-//     return getData('posts?filter[category_name]=post');
-// }
-// function userInfo() {
-//     return getData('posts?filter[category_name]=post');
-// }
-
-// function getData(url) {
-//     return $http
-//         .get(config.API_URL + url, { cache: true })
-//         .then(function(response) {
-//             if (response.data instanceof Array) {
-//                 var items = response.data.map(function(item) {
-//                     return decorateResult(item);
-//                 });
-//                 return items;
-//             } else {
-//                 return decorateResult(response.data);
-//             }
-//         });
-// }
-
-// return {
-//     allPosts: allPosts,
-//     allPostsByTag: allPostsByTag,
-//     allLatest: allLatest,
-//     allPostsBySearchTerm: allPostsBySearchTerm,
-//     featuredPosts: featuredPosts,
-//     post: post
-// };
-
-
-//}
-
 app.factory('FlickrService', function($http, $sce) {
     var url = "https://api.flickr.com/services/rest",
         apiKey = "5f1fc83583ecf2d1ba87d603691d3088",
@@ -76,35 +36,25 @@ app.factory('FlickrService', function($http, $sce) {
                 });
             })(),
 
-        // getAlbumPhotos: (function(albumId, response) {
-        //     var slides = [];
-        //     return $http({
-        //         method: 'GET',
-        //         url: url,
-        //         params: {
-        //             method: 'flickr.photosets.getPhotos',
-        //             api_key: apiKey,
-        //             user_id: userId,
-        //             photoset_id: albumId,
-        //             format: 'json',
-        //             nojsoncallback: 1
-        //         }
-        //     }).then(function(response) {
-        //         // console.log(response);
-        //         for (var i = response.data.photoset.photo.length - 1; i >= 0; i--) {
-
-        //             var slide = {
-        //                 src: 'https://farm'+response.data.photoset.photo[i].farm+'.static.flickr.com/'+response.data.photoset.photo[i].server+'/'+response.data.photoset.photo[i].id+'_'+response.data.photoset.photo[i].secret+'_b.jpg',
-        //                 w:500,
-        //                 h:400,
-        //                 pid: response.data.photoset.photo[i].id
-        //             };
-
-        //             slides.push(slide);
-        //         }
-        //         return slides;
-        //     });
-        // })(),
+        getAlbumPhotos: (function(albumId, response) {
+            
+            return $http({
+                method: 'GET',
+                url: url,
+                params: {
+                    method: 'flickr.photosets.getPhotos',
+                    api_key: apiKey,
+                    user_id: userId,
+                    photoset_id: albumId,
+                    format: 'json',
+                    nojsoncallback: 1
+                }
+            }).then(function(response) {
+                // console.log(response);
+                
+                return response.data;
+            });
+        })(),
 
         getAlbumLocation: (function(response) {
             var markers = [];
