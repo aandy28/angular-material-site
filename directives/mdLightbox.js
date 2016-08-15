36075904002391473e8b49249a22ecfa0c2560fd
aandy28 <aandy28@googlemail.com>
2016-08-15 14:01:41 +0100

    dialog and tabs added

--- directives/mdLightbox.js
+++ directives/mdLightbox.js
@@ -2,52 +2,59 @@ app.directive('mdLightbox', ['$mdDialog', function($mdDialog) {
     return {
         link: function($scope, elem, attrs) {
 
-            $scope.max = $scope.results.photoset.total - 1;
-            
-            $scope.selectedIndex = 0;
-
             elem.addClass('image-click');
-
+            $scope.newIndex = 0;
             elem.on('click', function() {
+                
+                // console.log($scope.allslides);
                 var image = attrs.src;
                 var title = attrs.mdLightboxTitle;
                 var index = attrs.mdIndex;
-                showLightboxModal(image, title, index);
+                var allSlides = $scope.allslides;
+                var maxSlides = $scope.allslides.length - 1;
+                console.log(index);
+                showLightboxModal(image, title, allSlides, maxSlides, index);
 
 
             });
 
             //Lightbox Modal
-            function showLightboxModal(image, title, index) {
+            function showLightboxModal(image, title, allSlides, maxSlides, index) {
                 var confirm = $mdDialog.confirm({
                     templateUrl: 'views/lightbox.html',
                     clickOutsideToClose: true,
                     controller: lightboxController
                 });
-
+                console.log(allSlides);
                 $mdDialog.show(confirm);
 
                 function lightboxController($scope, $mdDialog) {
                     $scope.image = image;
                     $scope.title = title;
-                    $scope.index = index;
+                    $scope.newIndex= index;
+                    $scope.lbSlides = allSlides;
+                    $scope.maxSlides = maxSlides;
 
-                    console.log($scope.index);
+
+                    console.log($scope.newIndex);
 
                     $scope.cancel = function() {
                         $mdDialog.cancel();
                     };
 
-                    $scope.nextImage = function(index) {
-                        var newindex = (index == $scope.max) ? 0 : index + 1;
-                        index = newindex;
-                        console.log(index);
-                    }
+                    $scope.nextTab = function() {
 
-                    $scope.prevImage = function(index) {
-                        var newindex = (index == $scope.max) ? 0 : index - 1;
-                        index = newindex;
-                    }
+                        var test = ($scope.newIndex == $scope.maxSlides) ? 0 : $scope.newIndex + 1;
+                        $scope.newIndex = test;
+
+                    };
+
+                    $scope.prevTab = function() {
+
+                        var test = ($scope.newIndex == $scope.maxSlides) ? 0 : $scope.newIndex - 1;
+                        $scope.newIndex = test;
+
+                    };
 
                 }
 
